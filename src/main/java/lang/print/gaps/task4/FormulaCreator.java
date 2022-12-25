088758265ec71fac17cc0b2d0e9941c9261612ca
Kirill Gusakov <justmymail003@gmail.com>
2022-12-25 15:29:13 +0300

    STUPID TASK

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -1,6 +1,9 @@
 package lang.print.gaps.task4;
 
+import java.util.Scanner;
+
 public class FormulaCreator {
     public static void main(String[] args) {
+        System.out.println("(9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2)");
     }
 }
